3805aaea877889252e53ec97ef8a18c6a570f6bc
Juergen Arne Klein <mail@juergen-arne-klein.de>
2025-05-08 16:27:18 +0200

    typo

--- docker-up.sh
+++ docker-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Stopping all containers."
 # docker stop $(docker ps -a -q) &> /dev/null
-docker compose down -v --remove-orpahns
+docker compose down -v --remove-orphans
 echo "Deleting all stopped containers."
 docker rm -f $(docker ps -a -q -f status=exited) &> /dev/null
 docker rm -f $(docker ps -a -q -f status=dead) &> /dev/null
