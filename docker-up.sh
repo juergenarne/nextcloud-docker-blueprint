d0d36d0bdd7c90de1c6ae192d158f0351557987e
Juergen Arne Klein <mail@juergen-arne-klein.de>
2025-05-08 16:23:42 +0200

    [shell] modify docker shell script

--- docker-up.sh
+++ docker-up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Stopping all containers."
-docker stop $(docker ps -a -q) &> /dev/null
+# docker stop $(docker ps -a -q) &> /dev/null
+docker compose down -v --remove-orpahns
 echo "Deleting all stopped containers."
 docker rm -f $(docker ps -a -q -f status=exited) &> /dev/null
 docker rm -f $(docker ps -a -q -f status=dead) &> /dev/null
