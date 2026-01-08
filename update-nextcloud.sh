361b4c814c94e302eac4203bb63cbda309a9aef0
juergenarne <juergenarne@users.noreply.github.com>
2026-01-08 11:39:26 +0100

    Update Nextcloud maintenance and upgrade script

--- update-nextcloud.sh
+++ update-nextcloud.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-docker exec -u www-data pnoom-nextcloud php occ maintenance:mode --on
+docker compose down -v --remove-orphans
+docker pull nextcloud:latest
+docker compose up -d --build
+sleep 10
+# docker exec -u www-data pnoom-nextcloud php occ maintenance:mode --on
 docker exec -u www-data pnoom-nextcloud php occ upgrade
-docker exec -u www-data pnoom-nextcloud php occ maintenance:mode --off
+docker exec -u www-data pnoom-nextcloud php occ maintenance:repair --include-expensive
+docker exec -u www-data pnoom-nextcloud php occ db:add-missing-indices
+# docker exec -u www-data pnoom-nextcloud php occ maintenance:mode --off
+docker exec -u www-data pnoom-nextcloud php occ status
