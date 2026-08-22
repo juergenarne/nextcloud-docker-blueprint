3afc211e86fd71d6529a58c19d1a03167d55f03a
juergenarne <juergenarne@users.noreply.github.com>
2026-08-22 19:39:11 +0200

    Update update-nextcloud.sh

--- update-nextcloud.sh
+++ update-nextcloud.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
 docker compose pull app
 docker compose up -d --force-recreate app
 
 docker compose exec -u www-data app php occ upgrade
+
 docker compose exec -u www-data app php occ maintenance:repair --include-expensive
 docker compose exec -u www-data app php occ db:add-missing-indices
 docker compose exec -u www-data app php occ db:add-missing-columns
 docker compose exec -u www-data app php occ db:add-missing-primary-keys
+
+docker compose exec -u www-data app php occ maintenance:mode --off
 docker compose exec -u www-data app php occ status
